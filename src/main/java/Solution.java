31f96f1714d8c927673763cbbee92522dfa33c3a
guoyitao <1004234893@qq.com>
2020-10-19 23:11:09 +0800

    树的非递归

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,28 +1,66 @@
+import sun.reflect.generics.tree.Tree;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
 
 
 public class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = m - 1;
-        int j = n - 1;
+    public int first = Integer.MAX_VALUE;
+    public int second = Integer.MAX_VALUE;
+    public int count = 0;
 
-        for (int k = m + n -1; k > 0; k--) {
-            if (nums1[i] > nums2[j]){
-                nums1[k] = nums1[i--];
-            }else if (nums1[i] < nums2[j]){
-                nums1[k] = nums2[j--];
-            }else if (i<0){
-                nums1[k] = nums2[j--];
-            }else{
-                nums1[k] = nums1[i--];
-            }
+    public int findSecondMinimumValue(TreeNode root) {
+        help(root);
+        return count == 0 ? -1 : second;
+    }
+
+    public void help(TreeNode root){
+        if (root == null){
+            return;
         }
+        if (root.val < first){
+            second = first;
+            first = root.val;
+        }else if (root.val <= second && root.val > first){
+            count++;
+            second = root.val;
+        }
+        help(root.left);
+        help(root.right);
 
-        return;
     }
 
     public static void main(String[] args) {
-//        ListNode l1 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4,new ListNode(5)))));
+        TreeNode treeNode1 = new TreeNode(2);
+        TreeNode treeNode2 = new TreeNode(2);
+        TreeNode treeNode3 = new TreeNode(5);
 
-        new Solution().merge(new int[]{1,2,3,0,0,0},3,new int[]{2,5,6},3);
+        TreeNode treeNode4 = new TreeNode(5);
+        TreeNode treeNode5 = new TreeNode(5);
+        treeNode1.left = treeNode2;
+        treeNode1.right = treeNode3;
+
+        treeNode3.left = treeNode4;
+        treeNode3.right = treeNode5;
+
+        new Solution().findSecondMinimumValue(treeNode1);
+
+
     }
 }

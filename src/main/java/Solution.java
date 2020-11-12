c6d5ead712f390e8446469dd73dd265d78984600
guoyitao <1004234893@qq.com>
2020-11-12 11:20:01 +0800

    贪心 字符串

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,5 +1,7 @@
 import sun.reflect.generics.tree.Tree;
 
+import java.util.*;
+
 class TreeNode {
     int val;
     TreeNode left;
@@ -22,32 +24,30 @@ class TreeNode {
 
 public class Solution {
 
-    public TreeNode trimBST(TreeNode root, int low, int high) {
-        if (root == null) return null;
-        if (root.val > high) return trimBST(root.left,low,high);
-        if (root.val < low) return trimBST(root.right,low,high);
-
-        root.left = trimBST(root.left,low,high);
-        root.right = trimBST(root.right,low,high);
-
-        return root;
+    public int longestPalindrome(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        int result = 0;
+        for (char c : s.toCharArray()) {
+            if (!map.containsKey(c)){
+                map.put(c,1);
+            }else{
+                map.put(c,map.get(c) +1);
+            }
+        }
+        int ji = 0;
+        for (Integer value : map.values()) {
+            if (value % 2 == 0){
+                result += value;
+            }else{
+                ji++;
+            }
+        }
+        return ji != 0 ?result +1 : result ;
     }
 
     public static void main(String[] args) {
-        TreeNode treeNode1 = new TreeNode(3);
-        TreeNode treeNode2 = new TreeNode(0);
-        TreeNode treeNode3 = new TreeNode(2);
-
-        TreeNode treeNode4 = new TreeNode(1);
-        TreeNode treeNode5 = new TreeNode(4);
-        treeNode1.left = treeNode2;
-        treeNode2.right = treeNode3;
-        treeNode3.left = treeNode4;
-
-        treeNode1.right = treeNode5;
-
-        new Solution().trimBST(treeNode1,1,3);
-
+        System.out.println(new Solution().longestPalindrome("aaacccccccaablk"));
 
     }
+
 }
